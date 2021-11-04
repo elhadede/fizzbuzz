2a52141109fb78fe37bf228ea0d6b1d82ba7720d
John Hammond <jling0139@gmail.com>
2021-11-04 12:09:01 +0300

    initial

---
+++ main.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        List=[]
+        for i in range(1,n+1):
+            print(i)
+            if i%3 == 0 and i%5 == 0:
+                List.append("FizzBuzz")
+            elif i%3 == 0:
+                List.append("Fizz")
+            elif i%5 == 0:
+                List.append("Buzz")
+            else:
+                List.append(str(i))
+        return List
+print(Solution.fizzBuzz(...,5))
